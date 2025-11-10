afe08e8e07458eae43aeb3a0dd06e5d925c83d76
Kennet Myllykoski <kennet.myllykoski@gmail.com>
2025-11-10 12:04:47 +0200

    1

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ with st.container(width='stretch'):
     st.markdown("""
                 <style>
                     div.stButton > button:first-child {
-background-color: #00cc00;color:white;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+background-color: #00cc00;color:black;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
                     }
                 </style>
 
